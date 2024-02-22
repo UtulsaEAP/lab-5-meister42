56343bc12e83387e74207628a4ac79457f201984
meister42 <hgh585@utulsa.edu>
2024-02-22 16:39:10 -0600

    problem 6

--- problem#6/fib.py
+++ problem#6/fib.py
@@ -1,6 +1,21 @@
-def fibonacci(n):
+def fibonacci(start_num):
     #write your code here
-    n = 1
+    old = 0
+    now = 1
+    new = 0
+    fiblist = [0, 1]
+    i = 0
+    for i in range(0, start_num):
+        new = old + now
+        old = now
+        now = new
+        fiblist.append(new)
+        i += 1
+
+    fibonum = fiblist[start_num]
+    return fibonum
+    
+
 
 
 if __name__ == '__main__':
