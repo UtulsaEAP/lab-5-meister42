e84544582124a6eed431b586817a05f7b9aa4d0c
meister42 <hgh585@utulsa.edu>
2024-02-22 14:58:41 -0600

    problem 5

--- problem#5/varSwap.py
+++ problem#5/varSwap.py
@@ -1,5 +1,17 @@
+"""
+Name: Henry Holman
+Time: thursday 2pm
+
+"""
+
+
+
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
-   #write your code here
+
+   user_val1 = user_input2
+   user_val2 = user_input1
+   user_val3 = user_input4
+   user_val4 = user_input3
    
    return user_val1, user_val2, user_val3, user_val4
 
@@ -9,6 +21,7 @@ if __name__ == '__main__':
    user_input3 = int(input())
    user_input4 = int(input())
    #store output for every input here
-   #print those output
-
+   out = swap_values(user_input1, user_input2, user_input3, user_input4)
+   print(str(out[0]) + " " + str(out[1]) + " " + str(out[2]) + " " + str(out[3]))
+   
  
