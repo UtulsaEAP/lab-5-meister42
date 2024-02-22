be1a5556eaec839d2fff221e2233f96b0f66e16e
meister42 <hgh585@utulsa.edu>
2024-02-22 14:20:08 -0600

    problem 3

--- Problem#3/stepCounter.py
+++ Problem#3/stepCounter.py
@@ -1,9 +1,17 @@
+"""
+Name: Henry Holman 
+Lab time: Thursday 2 pm
+"""
+
 def feet_to_steps(user_feet):
-    print()
-   #write your code here
+    steps = user_feet / 2.5
+    return steps
+   
 
 if __name__ == '__main__':
+
+    user_feet = float(input())
     #take input feet steps here
     #store it into the function
-    print()
-    #print your steps here
+    print(int(feet_to_steps(user_feet)))
+    
